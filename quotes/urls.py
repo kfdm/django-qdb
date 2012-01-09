cc255bc0efceb0d9e5e0dd73f34b66a1c9ed8760
Paul Traylor <kungfudiscomonkey@gmail.com>
2012-01-08 21:41:46 -0800

    Remove hardcoded URLs

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,14 +1,22 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, url
 from django.views.generic import list_detail
 from quotes.models import Quote
 
 urlpatterns = patterns('',
-    ('^/$', list_detail.object_list, {
-        'queryset': Quote.objects.all(),
-        'template_name': 'quote_list_page.html',
-        }),
-    ('^/recent/', list_detail.object_list, {
-        'queryset': Quote.objects.order_by('created_on'),
-        'template_name': 'quote_list_page.html',
-        })
+    url('^/$',
+        name='quotes_index',
+        view=list_detail.object_list,
+        kwargs={
+            'queryset': Quote.objects.all(),
+            'template_name': 'quote_list_page.html',
+            },
+        ),
+    url('^/recent/',
+        name='quotes_recent',
+        view=list_detail.object_list,
+        kwargs={
+            'queryset': Quote.objects.order_by('created_on'),
+            'template_name': 'quote_list_page.html',
+            },
+        )
 )
