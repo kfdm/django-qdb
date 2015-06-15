07e3ad76ce0222d0d45bb942c2cae472409a670d
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-06-15 23:22:38 +0900

    Bulk edit options

--- admin.py
+++ admin.py
@@ -5,5 +5,14 @@ from quotedb.models import Quote
 class QuoteAdmin(admin.ModelAdmin):
     list_display = ('body', 'owner', 'approved', 'created')
     list_filter = ('approved', 'owner',)
+    actions = ['mark_approved', 'mark_unapproved']
+
+    def mark_approved(self, request, queryset):
+        rows_updated = queryset.update(approved=True)
+        self.message_user(request, "%s successfully marked as approved." % rows_updated)
+
+    def mark_unapproved(self, request, queryset):
+        rows_updated = queryset.update(approved=False)
+        self.message_user(request, "%s successfully marked as approved." % rows_updated)
 
 admin.site.register(Quote, QuoteAdmin)
