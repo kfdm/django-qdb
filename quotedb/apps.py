32061acf75e91fb9fdfae36f5e3de9a7186ce8fc
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-08-07 17:25:12 +0900

    Some sample translation

--- quotedb/apps.py
+++ quotedb/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class QuoteDBConfig(AppConfig):
     name = 'quotedb'
-    verbose_name = "Quote Database"
+    verbose_name = _("Quote Database")
--- quotedb/models.py
+++ quotedb/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Quote(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    body = models.TextField()
-    owner = models.ForeignKey('auth.User', related_name='quotes')
-    approved = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_('created'))
+    body = models.TextField(verbose_name=_('body'))
+    owner = models.ForeignKey('auth.User', related_name='quotes', verbose_name=_('owner'))
+    approved = models.BooleanField(default=False, verbose_name=_('approved'))
 
     class Meta:
+        verbose_name = _('quote')
+        verbose_name_plural = _('quotes')
         ordering = ('-created',)
