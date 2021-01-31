a589f10b685233c421b8cc084617454142bbaf37
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-01-31 12:10:52 +0900

    Update to fix default timestamp

--- quotedb/models.py
+++ quotedb/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
 class Quote(models.Model):
-    created = models.DateTimeField(auto_now_add=True, verbose_name=_('created'))
+    created = models.DateTimeField(verbose_name=_("created"), default=timezone.now)
     body = models.TextField(verbose_name=_('body'))
     owner = models.ForeignKey('auth.User', verbose_name=_('owner'), on_delete=models.CASCADE)
     approved = models.BooleanField(default=False, verbose_name=_('approved'))
