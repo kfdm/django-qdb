3152304f6f5d25fd4191864df45d205925d2960d
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-01-31 12:01:56 +0900

    Use admin.RelatedOnlyFieldListFilter

--- quotedb/admin.py
+++ quotedb/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from quotedb.models import Quote
 
 
+@admin.register(Quote)
 class QuoteAdmin(admin.ModelAdmin):
     actions = ['mark_approved', 'mark_unapproved']
     list_display = ('body', 'owner', 'approved', 'created')
-    list_filter = ('approved', 'owner',)
+    list_filter = ('approved', ('owner', admin.RelatedOnlyFieldListFilter),)
 
     def mark_approved(self, request, queryset):
         rows_updated = queryset.update(approved=True)
@@ -14,5 +15,3 @@ class QuoteAdmin(admin.ModelAdmin):
     def mark_unapproved(self, request, queryset):
         rows_updated = queryset.update(approved=False)
         self.message_user(request, "%s successfully marked as approved." % rows_updated)
-
-admin.site.register(Quote, QuoteAdmin)
